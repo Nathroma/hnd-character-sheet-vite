ac6f43ec20d6fb6e62fc3a57499bc8237d538fcc
Nathan Romano <nathan.romano@meria.com>
2025-03-19 12:49:10 +0100

    pass whole character to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,28 @@
-import "./App.scss";
-import React from "react";
-import { statColors, statNames, StatType } from "./types/statType";
-import { ProficiencyLevel, skillAttributes, skillNames, SkillType } from "./types/skillType";
-import {
-  attributeColors,
-  attributeImgNames,
-  attributeTitles,
-  AttributeType,
-} from "./types/attributeType";
-import { ProfileType, ProfileNames } from "./types/profileType";
+import './App.scss';
+import React from 'react';
+import { StatType } from './types/statType';
+import { skillNames, SkillType } from './types/skillType';
+import { AttributeType } from './types/attributeType';
+import { ProfileType, ProfileNames } from './types/profileType';
 
-import StatBlock from "./components/mainStats/StatBlock";
-import SkillBlock from "./components/mainSkill/SkillBlock";
-import AttributeBlock from "./components/mainAttribute/AttributeBlock";
-import useCharacter from "./hooks/useCharacter";
-import ProfileBlock from "./components/mainprofile/ProfileBlock";
+import StatBlock from './components/mainStats/StatBlock';
+import SkillBlock from './components/mainSkill/SkillBlock';
+import AttributeBlock from './components/mainAttribute/AttributeBlock';
+import useCharacter from './hooks/useCharacter';
+import ProfileBlock from './components/mainprofile/ProfileBlock';
 
 function App() {
-
-  const {
-    character,
-    setStatValue,
-    setStatMastered,
-    switchSkillProficiencyLevel,
-    setAttributeValue,
-  } = useCharacter();  
+  const { character, setStatValue, setStatMastered, switchSkillProficiencyLevel, setAttributeValue } =
+    useCharacter();
 
   return (
     <div className="App">
       <div className="wrapper-profile-data">
         {Object.values(ProfileType).map((profileType) => (
-          <ProfileBlock
-            key={profileType}
-            label={ProfileNames[profileType]}
-            icon={undefined}
+          <ProfileBlock 
+          key={profileType} 
+          label={ProfileNames[profileType]} 
+          icon={undefined} 
           />
         ))}
       </div>
@@ -67,12 +56,8 @@ function App() {
               key={attributeType}
               character={character}
               attributeType={attributeType}
-              attributeTitle={attributeTitles[attributeType]}
-              color={attributeColors[attributeType]}
-              imgName={attributeImgNames[attributeType]} 
-              attributeValue={character.attributes[attributeType].value} 
-              onAttributeChange={(attributeValue) => setAttributeValue(attributeType, attributeValue)}         
-              />
+              onAttributeChange={(attributeValue) => setAttributeValue(attributeType, attributeValue)}
+            />
           ))}
         </div>
       </div>
--- src/components/mainAttribute/AttributeBlock.tsx
+++ src/components/mainAttribute/AttributeBlock.tsx
@@ -2,43 +2,35 @@ import React from "react";
 import "./AttributeBlock.scss";
 import NumberInput from "@/UI/numberInput/NumberInput";
 import { Character } from "@/types/characterType";
-import { AttributeType } from "@/types/attributeType";
+import { attributeColors, attributeImgNames, attributeTitles, AttributeType } from "@/types/attributeType";
 
 type SecondaryStatsProps = {
   character:Character
   attributeType:AttributeType
-  attributeTitle: string;
-  color: string;
-  imgName: string;
-  attributeValue: number
   onAttributeChange: (value: number) => void;
 };
 
 const AttributeBlock = ({
   character,
   attributeType,
-  attributeTitle,
-  color,
-  imgName,
-  attributeValue,
   onAttributeChange,
 }: SecondaryStatsProps) => {
-  const imgPath = `src/assets/icons/secondaryStatsLogos/${imgName}-logo.png`;
-  const imgAlt = `Logo stats ${attributeTitle}`;
+  const imgPath = `src/assets/icons/secondaryStatsLogos/${attributeImgNames[attributeType]}-logo.png`;
+  const imgAlt = `Logo stats ${attributeTitles[attributeType]}`;
 
   return (
     <div className="wrapper-block">
-      <div className="styled-shape" style={{ backgroundColor: color }}>
-        <label className="stats-name">{attributeTitle}</label>
+      <div className="styled-shape" style={{ backgroundColor: attributeColors[attributeType] }}>
+        <label className="stats-name">{attributeTitles[attributeType]}</label>
         <div className="input-background">
           <NumberInput
             className="number-input"
-            value={attributeValue}
+            value={character.attributes[attributeType].value}
             onChange={(e:any) => onAttributeChange(e.target.value)}
           />
         </div>
       </div>
-      <div className="circle" style={{ borderColor: color }}>
+      <div className="circle" style={{ borderColor: attributeColors[attributeType] }}>
         <img className="stats-logo" src={imgPath} alt={imgAlt} />
       </div>
     </div>
