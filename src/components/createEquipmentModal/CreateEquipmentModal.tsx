e97f23616a6ff84c55daa6ab155b14854714e9ff
Nathroma <39723452+Nathroma@users.noreply.github.com>
2025-04-07 18:41:05 +0200

    feat(inventoryPage): improve modal display

--- src/components/createEquipmentModal/CreateEquipmentModal.tsx
+++ src/components/createEquipmentModal/CreateEquipmentModal.tsx
@@ -43,37 +43,52 @@ const CreateEquipmentModal = ({ isOpen, onClose, onCreate }: CreateEquipmentModa
         <div className="create-equipment-modal">
             <div className="modal-content">
                 <h2>Créer un nouvel équipement</h2>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Nom"
-                    value={equipment.name}
-                    onChange={handleChange}
-                />
-                <select
-                    name="action"
-                    onChange={handleChange}
-                >
-                    {Object.values(EquipementAction).map((action) => (
-                        <option key={action} value={action}>
-                            {action}
-                        </option>
-                    ))}
-                </select>
-                <input
-                    type="number"
-                    name="quantity"
-                    placeholder="Quantité"
-                    value={equipment.quantity}
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    name="weight"
-                    placeholder="Poids"
-                    value={equipment.weight}
-                    onChange={handleChange}
-                />
+                <div className="div-input">
+                    <span>
+                        Nom de l'équipement
+                    </span>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Nom"
+                        value={equipment.name}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="div-input">
+                    <span>Type d'action</span>
+                    <select
+                        name="action"
+                        onChange={handleChange}>
+                        {Object.values(EquipementAction).map((action) => (
+                            <option key={action} value={action}>
+                                {action}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="div-input">
+                    <span>
+                        Quantité
+                    </span>
+                    <input
+                        type="number"
+                        name="quantity"
+                        placeholder="Quantité"
+                        value={equipment.quantity}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="div-input">
+                    <span>Poid d'une unité</span>
+                    <input
+                        type="text"
+                        name="weight"
+                        placeholder="Poids"
+                        value={equipment.weight}
+                        onChange={handleChange}
+                        />
+                </div>
                 <div className="modal-actions">
                     <button className="cancel" onClick={onClose}>Annuler</button>
                     <button className="validate" onClick={handleSubmit}>Créer</button>
